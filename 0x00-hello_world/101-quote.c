f8e83dc1a776e00c93bea8b1d0ae21600378f113
Dave <emenyonuuzo@gmail.com>
2022-09-08 20:28:30 +0000

    Printing to standard error with fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-fprintf
-	(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ");
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		, 5, 31,  stderr);
 	return (1);
 }
