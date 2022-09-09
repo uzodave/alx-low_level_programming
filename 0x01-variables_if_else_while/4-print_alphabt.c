7ee6157ab957fd4e33c93345ae994f3423072fe4
Dave <emenyonuuzo@gmail.com>
2022-09-09 07:15:56 -0700

    Excluding certain letters using the continue statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if ((i == 'q') || (i == 'e'))
+		{
+		continue;
+	}
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
+}
