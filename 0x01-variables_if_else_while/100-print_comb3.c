c9a699a41deaca2bf7dc2ea004d71adc571dc800
Dave <emenyonuuzo@gmail.com>
2022-09-10 20:33:41 +0000

    Putchar advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (!(j <= i))
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
