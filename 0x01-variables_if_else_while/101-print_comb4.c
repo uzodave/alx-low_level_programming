ab61510cbc20472b8feb68a21c3aa2bfb114f102
Dave <emenyonuuzo@gmail.com>
2022-09-10 21:10:31 +0000

    More complex putchar task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && i < k && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+				if (!(i == '7' && j == '8' && k == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				}
+				}
+			}
+	}
+		putchar('\n');
+	return (0);
+}
