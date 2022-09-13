8e247f1f8f8c1139924d8ff7a15edb6959b05202
Dave <emenyonuuzo@gmail.com>
2022-09-13 16:56:38 +0000

    Switching to absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check the code and print the absolute value of an integer
+ * @n: variable for integers
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+	return (0);
+	}
