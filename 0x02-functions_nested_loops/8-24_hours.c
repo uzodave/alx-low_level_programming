fc8f74560dab43c450b13ddf60df98b2086f46b3
Dave <emenyonuuzo@gmail.com>
2022-09-13 20:27:39 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all hours from 00:00 to 23:59
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 24; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+
+		for (j = 0; i < 60; i++)
+		{
+			_putchar(a + '0');
+			_putchar(b + '0');
+			_putchar(':');
+
+			c = j / 10;
+			d = j % 10;
+
+			_putchar(c + '0');
+			_putchar(d + '0');
+			_putchar('\n');
+		}
+	}
+}
