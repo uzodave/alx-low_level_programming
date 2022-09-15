bc027fe2217914eafe206dba2a34b7ee1d8ef7d5
Dave <emenyonuuzo@gmail.com>
2022-09-15 23:18:51 +0000

    Prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,19 +5,27 @@
  *   *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 	long int n = 612852475143;
 	long int i;
-		
+	long int large_prime = 1;
+
 	for (i = 2; i < n; i++)
 	{
-		if ((n % 1) == 0)
+		if (n % i == 0)
 		{
-			n = n / i;
+			large_prime *= i;
+			n /= i;
+			if (n == 1)
+			{
+				printf("%lu\n", i);
+				return (0);
 		}
+			i = 1;
 	}
-		printf ("%ld\n ", i);
-			return 0;
+	}
+
+			return (0);
 }
 
