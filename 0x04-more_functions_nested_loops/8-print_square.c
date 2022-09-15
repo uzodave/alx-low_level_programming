317a322c215401d52884c0c14f34254372019b6b
Dave <emenyonuuzo@gmail.com>
2022-09-15 18:45:34 +0000

    Printing a square with pound sign to stdout

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_square- prints a square using # sign
+ * print_square - prints a square using # sign
  * onto the terminal
  * @size: the determinant for how big the square will be
  * Return: Always jackshit
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i;
 	int j;
 
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= n; j++)
+		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
 	}
+	if (size <= 0)
+		_putchar('\n');
 }
