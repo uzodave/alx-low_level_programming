9c6880672904477c34339ff26b327d183b58b5e9
Dave <emenyonuuzo@gmail.com>
2022-09-23 11:52:20 +0000

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 
 	if (n > 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		i = n * -1;
 	}
 	else
@@ -30,7 +30,7 @@ void print_number(int n)
 
 		for (; k >= 1; k /= 10)
 		{
-			_putchar(((i / k) % 10) + '0');
-	}
+			_putchar(((i / k) % 10) + 48);
+		}
 
 }
