31893cba5ea89e36ba9b5bf1a34ba4995de27123
Dave <emenyonuuzo@gmail.com>
2022-09-23 13:23:37 +0000

    Magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * magic integer
+ * @n: integer to be input
+ * Return: void
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5);
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
