a72212d473391fad0fde7227d5f4bee302b9bad2
Dave <emenyonuuzo@gmail.com>
2022-09-26 21:44:01 +0000

    Locate a string using strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string
+ * the function returns a pointer to the
+ * first occurence of a character in a string
+ * @s: pointer to the character string
+ * @c: the character itself
+ * Return: a pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
