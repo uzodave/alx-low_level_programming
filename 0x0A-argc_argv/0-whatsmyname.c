efa829922d063e4a6fd6d27103ed53916cac0bfd
Dave <emenyonuuzo@gmail.com>
2022-09-30 14:16:18 +0000

    Arguments to main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Description: the program uses arguments to main
  * Return: 0 for success
  */
-int main(int __attribute__ ((unused)) argc, char **argv)
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	/*(void)argc;*/
 	printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int __attribute__ ((unused)) argc, char **argv);
+int main(int __attribute__ ((unused)) argc, char *argv[])
+#endif
