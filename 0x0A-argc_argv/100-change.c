cca7a0af88008a3c5f31b99e2632aba5fc77ade8
Dave <emenyonuuzo@gmail.com>
2022-09-30 21:53:13 +0000

    Temp

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: count of arguments in the program
+ * @argv: array of pointers to strings(the arguments)
+ * Description: this program uses arguments to main
+ * (command line arguments)
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	/**
+	 * int i;
+	 * int cents;
+	 */
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
