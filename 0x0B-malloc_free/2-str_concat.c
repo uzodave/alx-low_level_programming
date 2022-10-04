d9ae1a08ee68cfe349154286504c9671e0f73f1b
Dave <emenyonuuzo@gmail.com>
2022-10-04 14:30:59 +0000

    Concatenating task with Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,22 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - finds the length of a string
- * @s: input string
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	if (s == NULL)
-		s = "";
-	return (0);
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
-/**
  * str_concat - returns a pointer to a newly allocated space
  * in memory which contains the contents of the strings s1 and s2
  * given as parameters
@@ -29,23 +13,31 @@ int _strlen(char *s)
 char *str_concat(char *s1, char *s2)
 {
 	char *ptConcat;
-	int i, j;
+	int i, j, a, b;
 
-	/*finding the lengths of s1 and s2*/
-	i = _strlen(s1);
-	j = _strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
 	/* to determine size and allocate enough memory space */
-	ptConcat = (char *)malloc(i + j + 1);
+	ptConcat = (char *)malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (ptConcat == NULL)
 	{
 		return (NULL);
 	}
 	/*to concatenate the strings to the new address*/
-	for (i = 0; s1[i] != '\0'; i++)
-		ptConcat[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; j++, i++)
-		ptConcat[i] = s2[j];
-	ptConcat[i] = '\0';
+	for (a = 0, b = 0; a < (i + j + 1); a++)
+	{
+		if (a < i)
+			ptConcat[a] = s1[a];
+		else
+			ptConcat[a] = s2[b++];
+	}
 	return (ptConcat);
 }
