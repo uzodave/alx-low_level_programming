0600d2e396cdbf5aba2d8307eb564249d5bfebd3
Dave <emenyonuuzo@gmail.com>
2022-10-05 12:59:40 +0000

    Change maker

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,24 +3,52 @@
 #include <ctype.h>
 
 /**
- * main - adds positive numbers
+ * main - prints the minimum numbers of coins to make change
  * @argc: count of arguments in the program
  * @argv: array of pointers to strings(the arguments)
  * Description: this program uses arguments to main
  * (command line arguments)
  * Return: 0 for success
  */
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[])
 {
-	/**
-	 * int i;
-	 * int cents;
-	 */
+	int cents, coins = 0;
 
-	if (argc != 2)
+	if (argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins);
+
 	return (0);
+
 }
