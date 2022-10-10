49f7cfcf322868f570dbdaeec33a853fcd3786a9
Dave <emenyonuuzo@gmail.com>
2022-10-10 19:09:48 +0000

    Beginning structs

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+#Structures and Typedef
+Structures are user-defined data types that combine data items of different types
+Typedef helps prevent redundancy in typing "struct" when creating structs
+Interesting init?
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog struct
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG*/
