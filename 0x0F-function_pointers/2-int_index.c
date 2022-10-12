9ebd95eb37e5de68a93f6a9300fca308d7f77ff0
Dave <emenyonuuzo@gmail.com>
2022-10-12 20:59:08 +0000

    Function Pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 /**
  * int_index - a function that searches for an integer
  * @array: where the elements are contained
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
